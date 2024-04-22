b02d36a4bd5a0f2232bab8ea2ac021bdfcdd1205
Guvanch Yazgulyyev <77998139+GuvanchYazgulyyev@users.noreply.github.com>
2024-04-22 23:39:38 +0300

    Last End

--- Rest Api Connect MAUI/MauiProgram.cs
+++ Rest Api Connect MAUI/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using MAUI.WebApi.Interface;
+using Microsoft.Extensions.Logging;
 
 namespace Rest_Api_Connect_MAUI
 {
@@ -13,6 +14,27 @@ namespace Rest_Api_Connect_MAUI
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+            builder.Services.AddSingleton<IPlatforumHttpMessageHandler>(_ =>
+                {
+#if ANDROID
+                    return new Platforms.Android.AndroidHttpMessageHandler();
+#elif IOS
+                    return new Platforms.iOS.IosHttpMessageHandler();
+#endif
+                });
+            builder.Services.AddHttpClient("maui-to-https-localhost", httpClient =>
+            {
+                var bseAddress = DeviceInfo.Platform == DevicePlatform.Android
+                ? "https://10.0.2.2:12345"
+                : "https://localhost:12345";
+                httpClient.BaseAddress = new Uri(bseAddress);
+            })
+                .ConfigureHttpMessageHandlerBuilder(httpClient =>
+                {
+                    var platforumHttpMessageHandler = httpClient.Services
+                    .GetRequiredService<IPlatforumHttpMessageHandler>();
+                    httpClient.PrimaryHandler = platforumHttpMessageHandler.GetHttpMessageHandler();
                 });
             // builder.Services.AddHttpClient("api", httpClient=>httpClient.BaseAddress=new Uri("https://localhost:7063/WeatherForecast"));
 #if DEBUG
